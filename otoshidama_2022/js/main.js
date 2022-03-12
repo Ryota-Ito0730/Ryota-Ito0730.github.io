c413e4aed33a318a56b7ec80d073177a0ed2fda3
Ryota-Ito0730 <rgitmain01@gmail.com>
2022-03-12 21:35:22 +0900

    制限時間を30秒に変更

--- otoshidama_2022/js/main.js
+++ otoshidama_2022/js/main.js
@@ -12,7 +12,7 @@ window.addEventListener('load', function () {
   // ボタン連打初期値
   let i = 0;
   // ここの時間(秒換算)をページ内に残り～秒として表示させる大元の数値とする/setTimeout内と共用
-  const millisecond = 3000;
+  const millisecond = 30000;
   // ボタン連打ができる残時間
   let time = millisecond / 1000;
   let j = 0;
@@ -25,7 +25,7 @@ window.addEventListener('load', function () {
 
   function timeCounter() {
     let timeoutID = setTimeout(timeCounter, 1000);
-    if (j === 3) {
+    if (j === time) {
       clearTimeout(timeoutID);
       btn_stopp_after_reading();
     }
